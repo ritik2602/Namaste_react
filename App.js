acbca7d0c78952136a5f7779eb3a50aa105f0e8c
Ritik Gupta <ritikgupta2602@gmail.com>
2024-05-07 16:43:59 +0530

    react deployement using parcel

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
-/**
- * 
- * <div id="parent">
- *      <div id="child">
- *          <h1>..</h1>
- *      </div>
- * </div>
- * 
- * 
- */
-const parent=React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},//[react.createElement("h1",{},""),react.createElement("h1",{},""),react.createElement("h1",{},"")....]
-React.createElement("h1",{},"I'm in h1"))
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//jsx?
-
-// const heading=React.createElement("h1",{id:"heading"},"Hello world from react")//to give attributes to your tag->{}
-//heading is react element or js object
-const root=ReactDOM.createRoot( document.getElementById("root"));
+const parent=React.createElement("div",{id:"parent"},[
+React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I am in h1"),
+    React.createElement("h2",{},"I am in h2"),
+    React.createElement("h1",{},"I am again in h1")]),
+    React.createElement("h1",{},"I'm in h1")
+]);
 console.log(parent)
+    
+const root=ReactDOM.createRoot( document.getElementById("root"));
+
 root.render(parent);//render-> to take this obj to create h1 tag  in html and put it in root
-
-
-//package.json is a configuration for npm
+    
